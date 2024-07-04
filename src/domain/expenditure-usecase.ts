85a9dbf3c245f04949f61416109849cb2764b577
BradleyG2004 <129465283+BradleyG2004@users.noreply.github.com>
2024-07-04 12:47:53 +0200

    some updates

--- src/domain/expenditure-usecase.ts
+++ src/domain/expenditure-usecase.ts
@@ -14,29 +14,32 @@ export class ExpenditureUsecase {
 
     async listExpenditure(listExpenditureFilter: ListExpenditureFilter): Promise<{ expenses: Expenditures[]; totalCount: number; } | string> {
         console.log(listExpenditureFilter);
-        const query = this.db.getRepository(Expenditures).createQueryBuilder('expenditures');
-
+        const query = this.db.getRepository(Expenditures)
+            .createQueryBuilder('expenditures')
+            .leftJoinAndSelect('expenditures.user', 'user');
+    
         if (listExpenditureFilter.id) {
             const user = await this.db.getRepository(User)
                 .createQueryBuilder('user')
                 .where('user.id = :id', { id: listExpenditureFilter.id })
                 .andWhere('user.isDeleted = false')
                 .getOne();
-
+    
             if (!user) {
                 return `Nothing Found !!! from userId: ${listExpenditureFilter.id}`;
             }
-
+    
             query.andWhere('expenditures.userId = :userId', { userId: listExpenditureFilter.id });
         }
-
+    
         query.skip((listExpenditureFilter.page - 1) * listExpenditureFilter.limit)
             .take(listExpenditureFilter.limit);
-
+    
         const [expenses, totalCount] = await query.getManyAndCount();
         return {
             expenses,
             totalCount
         };
     }
+    
 }
